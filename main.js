e562222ad9e76782af90679d06b285b4d501d0d4
Ehsan <ehsan.shahbazicode@gmail.com>
2024-04-03 19:15:59 +0330

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ const cell_size = 1; // اندازه سلول ها
 
 let current_population = []; // جمعیت کنونی
 let next_population = []; // نسل جدید
+
+const initialize = () => {
+  for (let y = 0; y < height; y++) {
+    // برا هر خونه آرایه، یه آرایه دیگه ایجاد میکنیم
+    current_population[y] = [];
+    next_population[y] = [];
+    for (
+      let x = 0;
+      x < width;
+      x++ // مقدار 0 یا 1 به جمعیت ها میدیم
+    )
+      current_population[y][x] = Math.floor(Math.random() * 2);
+  }
+};
